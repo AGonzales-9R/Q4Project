be237c53dca63bffc9c899a52537c1782d0bd94f
AGonzales-9R <gonzales.am@obmontessori.edu.ph>
2025-05-23 16:04:18 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 function showPass() {
   var x = document.getElementById("inputPass");
+  
   if (x.type === "password") {
     x.type = "text";
+    
   } else {
     x.type = "password";
   }
 }
+
+window.alert("♦️ LOADING ♦️ ");
